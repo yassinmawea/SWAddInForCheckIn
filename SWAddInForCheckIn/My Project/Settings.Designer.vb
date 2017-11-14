929aede9e59cc75814e3323c650386aff81e63b8
yassinmawea <32625302+yassinmawea@users.noreply.github.com>
2017-11-14 11:35:27 +0800

    assembly name change to small sw

--- SWAddInForCheckIn/My Project/Settings.Designer.vb
+++ SWAddInForCheckIn/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.SWAddInForCheckIn.My.MySettings
+        Friend ReadOnly Property Settings() As Global.swAddInForCheckIn.My.MySettings
             Get
-                Return Global.SWAddInForCheckIn.My.MySettings.Default
+                Return Global.swAddInForCheckIn.My.MySettings.Default
             End Get
         End Property
     End Module
