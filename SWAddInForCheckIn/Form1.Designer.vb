b11b30b3814c08a82d25dad48aa333e69afefd74
yassinmawea <32625302+yassinmawea@users.noreply.github.com>
2017-11-29 17:07:46 +0800

    Final

--- SWAddInForCheckIn/Form1.Designer.vb
+++ SWAddInForCheckIn/Form1.Designer.vb
@@ -23,22 +23,34 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(133, 61)
+        Me.Label1.Location = New System.Drawing.Point(134, 42)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(187, 17)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Derived Output in progress.."
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 94)
+        Me.ProgressBar1.Maximum = 10
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(437, 23)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee
+        Me.ProgressBar1.TabIndex = 2
+        Me.ProgressBar1.Value = 2
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(461, 143)
+        Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.Label1)
         Me.MaximizeBox = False
         Me.Name = "Form1"
@@ -48,4 +60,5 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
 End Class
--- SWAddInForCheckIn/Form1.vb
+++ SWAddInForCheckIn/Form1.vb
@@ -10,4 +10,13 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Public Sub DrawingCount(num As Integer)
+        ProgressBar1.Maximum = num
+    End Sub
+
+    Public Sub increaseValue()
+        ProgressBar1.Value = ProgressBar1.Value + 1
+    End Sub
+
 End Class
