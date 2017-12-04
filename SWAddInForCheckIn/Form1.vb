51ba2959d539dcf6644b6abbe272627b6d6a1fb5
yassinmawea <32625302+yassinmawea@users.noreply.github.com>
2017-12-04 13:34:11 +0800

    temp change

--- SWAddInForCheckIn/Form1.vb
+++ SWAddInForCheckIn/Form1.vb
@@ -1,17 +1,31 @@
 ﻿Public Class Form1
-    Dim MaxValue As Integer
+    Dim MaxValue As Integer = 10
     Dim percentage As Integer
     Dim currentValue As Integer = 0
 
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         CenterToScreen()
-        BackgroundWorker1.RunWorkerAsync()
     End Sub
 
     Public Sub Open()
         Show()
         Refresh()
+
+        Do
+            System.Threading.Thread.Sleep(1000)
+            currentValue = ProgressBar1.Value
+            currentValue = currentValue + 1
+
+            ProgressBar1.Value = currentValue
+
+            If currentValue = 10 Then
+                currentValue = 1
+            End If
+
+        Loop While ProgressBar1.Value <= 10
+
+
 
         'If Not BackgroundWorker1.IsBusy = True Then
         'BackgroundWorker1.RunWorkerAsync()
@@ -31,15 +45,6 @@
         'ProgressBar1.Increment(1)
     End Sub
 
-    Private Sub BackgroundWorker1_DoWork(sender As Object, e As ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
-        System.Threading.Thread.Sleep(1000)
-        'currentValue = currentValue + 1
-        'percentage = (currentValue / MaxValue)
-        'BackgroundWorker1.ReportProgress(percentage)
-    End Sub
 
-    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
-        'ProgressBar1.Value = e.ProgressPercentage
-    End Sub
 
 End Class
