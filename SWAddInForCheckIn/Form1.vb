00744a01c4b175e7c48814c1a71c0301d622cdb2
yassinmawea <32625302+yassinmawea@users.noreply.github.com>
2017-12-07 20:29:27 +0800

    addded background worker

--- SWAddInForCheckIn/Form1.vb
+++ SWAddInForCheckIn/Form1.vb
@@ -10,34 +10,13 @@
 
     Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
         TopMost = True
-        Debug.Print("In Open ")
-        Try
+
+        BackgroundWorker1.WorkerSupportsCancellation = True
+        BackgroundWorker1.WorkerReportsProgress = True
+
+        BackgroundWorker1.RunWorkerAsync()
 
 
-            Show()
-            Refresh()
-
-            Do
-                System.Threading.Thread.Sleep(1000)
-                'currentValue = ProgressBar1.Value
-                currentValue = currentValue + 2
-
-                ProgressBar1.Value = currentValue
-                Refresh()
-
-                Debug.Print("currentValue -> " & currentValue)
-
-
-                If currentValue = 99 Then
-                    currentValue = 1
-                End If
-
-            Loop Until currentValue = 111
-
-        Catch ex As Exception
-            Close()
-
-        End Try
     End Sub
 
     Public Sub Open()
@@ -88,6 +67,41 @@
         'ProgressBar1.Increment(1)
     End Sub
 
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Debug.Print("In Open ")
+        Try
+
+            Show()
+            Refresh()
+
+            Do
+                System.Threading.Thread.Sleep(1000)
+                'currentValue = ProgressBar1.Value
+                currentValue = currentValue + 2
+
+                BackgroundWorker1.ReportProgress(currentValue)
 
 
+                Refresh()
+
+                Debug.Print("currentValue -> " & currentValue)
+
+
+                If currentValue = 96 Then
+                    currentValue = 1
+                End If
+
+            Loop Until currentValue = 98
+
+        Catch ex As Exception
+            Close()
+
+        End Try
+
+
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
 End Class
